80c15329075b3129034ba06e7372ffbdd6f0fbeb
Jaden Lund <lundjaden@gmail.com>
2022-07-19 08:45:43 -0500

    commit tree 89d4d650685d60dc676489fc330d4d3f9d4d4fa6

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+let feetTraveled;
+function distanceFromHqInBlocks(blocks){
+    if (blocks < 42){
+        return (42 - blocks)
+    }
+    else return (blocks - 42);
+}
+
+function distanceFromHqInFeet(blocks) {
+    return (distanceFromHqInBlocks(blocks) * 264);
+}
+
+function distanceTravelledInFeet(start, end){
+    if(start > end){
+        return  (start - end) * 264;
+    }
+    else return  (end - start) * 264;
+    
+}
+
+function calculatesFarePrice(start, end){
+    if(distanceTravelledInFeet(start, end) <= 400){
+        return 0;
+    }
+    else if (distanceTravelledInFeet(start, end) < 2000)
+    {
+        return (distanceTravelledInFeet(start, end) - 400) * .02;
+    }
+    else if (distanceTravelledInFeet(start, end) > 2500)
+    {
+        return 'cannot travel that far';
+    }
+    else return 25;
+}
